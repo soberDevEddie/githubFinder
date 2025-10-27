5650365e9a9789fbd3f2c7f4ab53e045b1c84dda
EddieO23 <eocampo52@gmail.com>
2025-10-27 18:36:27 -0500

    refactored code for recentSuggestions

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -5,6 +5,7 @@ import { useDebounce } from 'use-debounce';
 import { fetchGithubUser, searchGithubUser } from '../api/github';
 import UserCard from './UserCard';
 import RecentSearches from './RecentSearches';
+import SuggestionDropdown from './SuggestionDropdown';
 import type { GithubUser } from '../types';
 
 const UserSearch = () => {
@@ -71,30 +72,28 @@ const UserSearch = () => {
           />
 
           {showSuggestions && suggestions?.length > 0 && (
-            <ul className='suggestions'>
-              {suggestions.slice(0, 5).map((user: GithubUser) => (
-                <li
-                  key={user.login}
-                  onClick={() => {
-                    setUsername(user.login);
-                    setShowSuggestions(false);
+            <SuggestionDropdown
+              suggestions={suggestions}
+              show={showSuggestions}
+              onSelect={(selected) => {
+                setUsername(selected);
+                setShowSuggestions(false);
 
-                    if (submittedUsername !== user.login) {
-                      setSubmittedUsername(user.login);
-                    } else {
-                      refetch();
-                    }
-                  }}
-                >
-                  <img
-                    src={user.avatar_url}
-                    alt={user.login}
-                    className='avatar-xs'
-                  />
-                  {user.login}
-                </li>
-              ))}
-            </ul>
+                if (submittedUsername !== selected) {
+                  setSubmittedUsername(selected);
+                } else {
+                  refetch();
+                }
+
+                setRecentUsers((prev) => {
+                  const updated = [
+                    selected,
+                    ...prev.filter((user) => user !== selected),
+                  ];
+                  return updated.slice(0, 5);
+                });
+              }}
+            />
           )}
         </div>
         <button type='submit'>Search</button>

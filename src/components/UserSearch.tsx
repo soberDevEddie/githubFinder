34df71f0ddc61bf32c24e56ffadb6f16f2b0ec7f
EddieO23 <eocampo52@gmail.com>
2025-10-27 17:57:36 -0500

    added locastorage functionality to app. saving recent searches to localstorage

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 import { fetchGitubUser } from '../api/github';
@@ -8,7 +8,11 @@ import RecentSearches from './RecentSearches';
 const UserSearch = () => {
   const [username, setUsername] = useState('');
   const [submittedUsername, setSubmittedUsername] = useState('');
-  const [recentUsers, setRecentUsers] = useState<string[]>([]);
+  const [recentUsers, setRecentUsers] = useState<string[]>(()=> {
+    const stored = localStorage.getItem('recentUsers');
+
+    return stored ? JSON.parse(stored) : [];
+  });
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['users', submittedUsername],
@@ -32,6 +36,10 @@ const UserSearch = () => {
       return updated.slice(0, 5);
     });
   };
+
+  useEffect(() => {
+    localStorage.setItem('recentUsers', JSON.stringify(recentUsers));
+  }, [recentUsers]);
 
   return (
     <>
